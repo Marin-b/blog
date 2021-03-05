6b37df48cd094a0bd200f5a1fd99f26c70ad24e5
Marin Bibot <marin.bibot@hotmail.be>
2021-03-05 10:40:32 +0100

    Cloudinary and active storage setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "open-uri"
+
+Article.destroy_all
+article = Article.create(title: 'NES', body: "A great console")
+
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+article.photos.attach(io: file, filename: 'Nes.png')
